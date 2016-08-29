4af704c1390fa92fa7b45ec9cde7a51def85a945
muhammadyehia <muhammad.yehia.elsayed@gmail.com>
2016-08-29 16:23:09 +0200

    add contact list component

---
+++ app/contactList/contactList.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+     moduleId: module.id,
+    selector: 'contact-list',
+    templateUrl: 'contactList.component.html'
+})
+export class ContactListComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { 
+
+    }
+
+}
